acad07e028346910194be6d3e4b5fd1fc9787898
ramzon-renz <ramzonrenzgamba@gmail.com>
2025-03-11 14:05:15 +0800

    Creating auth screens

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React from 'react'
+import  Link  from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+
+interface AuthLayoutProps {
+    children: React.ReactNode;
+}
+const AuthLayout = ({ children } : AuthLayoutProps) => {
+    const pathname = usePathname();
+    const isSignIn = pathname === "/sign-in";
+
+  return (
+    <main className='bg-neutral-100 min-h-screen'>
+        <div className='mx-auto max-w-screen-2xl p-4'>
+            <nav className='flex justify-between items-center'>
+                <Image src="/logo.svg" height={56} width={152} alt='Logo'/>
+                <div className='flex items-center gap-2'>
+                   <Button asChild variant="secondary">
+                        <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                            {isSignIn ? "Sign Up" : "Login"}
+                        </Link>
+                   </Button>
+                </div>
+            </nav>
+            <div className='flex flex-col items-center justify-center pt-4 md:pt-14'>
+                {children}
+            </div>   
+        </div>
+    </main>
+  )
+}
+
+export default AuthLayout
